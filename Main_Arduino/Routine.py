f76ba03f1539fde894a8562ff25b5d5e935920d4
selinasuzeroglu <selina.suzeroglu@gmail.com>
2023-01-16 16:59:45 +1100

    Commit

--- Main_Arduino/Routine.py
+++ Main_Arduino/Routine.py
@@ -1,24 +1,25 @@
 import time
 
 from Positioning import positioning_MTP, homing_MTP
-from Trigger_InProcess_Input import microswitch
-from Trigger_InProcess_Output import fire_signal
-from Nikon_Remote_Control.single_snapshot import snapshot
+# from Trigger_InProcess_Input import microswitch
+# from Trigger_InProcess_Output import fire_signal
+from Camera import *
+
 
 horizontal = 50
 vertical = 50
 sample_distance = 32.5
 
+camera_control_cmd_path = 'C:\\Program Files (x86)\\digiCamControl\\CameraControlCmd.exe'
+camera = Camera(control_cmd_location=camera_control_cmd_path)
+
 
 homing_MTP()
 positioning_MTP(vertical, horizontal)
-time.sleep(1)
-snapshot()
+camera.capture_single_image(autofocus=True)
 positioning_MTP(vertical, horizontal + sample_distance)
-time.sleep(1)
-snapshot()
+camera.capture_single_image(autofocus=True)
 positioning_MTP(vertical, horizontal + 2*sample_distance)
-time.sleep(1)
-snapshot()
+camera.capture_single_image(autofocus=True)
 homing_MTP()
 
