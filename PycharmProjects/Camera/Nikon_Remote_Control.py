034c83edf9e040815633129c51d91912d15688b9
selinasuzeroglu <114384134+selinasuzeroglu@users.noreply.github.com>
2023-01-25 15:21:26 +1100

    Delete PycharmProjects directory

--- PycharmProjects/Camera/Nikon_Remote_Control.py
+++
@@ -1,6 +0,0 @@
-
-from os import system
-
-if __name__ == '__main__':
-    system('pip uninstall DigiCam -y')
-    system('pip install .')
--- PycharmProjects/UVVis_3.0/Main_PLC/PLC.py
+++
@@ -1,7 +0,0 @@
-import snap7
-
-plc = snap7.client.Client()
-plc.connect("192.168", 0, 1)
-
-
-
