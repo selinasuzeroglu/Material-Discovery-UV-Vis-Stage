475ee615677f9c562474572b34aa7a2d45aa7947
selinasuzeroglu <selina.suzeroglu@gmail.com>
2023-02-23 16:49:30 +1100

    Commit

--- Plots_Analysis.py
+++ Plots_Analysis.py
@@ -1,7 +1,11 @@
 import pyodbc
 import pandas as pd
-import math
+
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+from scipy.signal import savgol_filter
+
 
 cnxn_str = ("Driver={SQL Server};"
             "Server=MU00195249\ZEISSSQL;"
@@ -36,30 +40,84 @@ class InProcessData:
 
 transmission = InProcessData('Transmission', values).get_data()
 reflection = InProcessData('Reflection', values).get_data()
+absorbance = InProcessData('Absorbance', values).get_data()
 wavelength_transmission = InProcessData('Transmission', wavelengths).get_data()
 wavelength_reflection = InProcessData('Reflection', wavelengths).get_data()
 
 
-def get_energy(λ):  # get Energy in eV
-    energy = (6.626070e-34 * 299792458) / (λ * 1e-9) * 6.242e18
+def get_energy(λ):  # λ = Wavelengths
+    energy = (6.626070e-34 * 299792458) / (λ * 1e-9) * 6.242e18  # get Energy in eV
     return energy
 
 
-def get_alpha(A, d):  # A = Absorbance, d = thickness of the sample (in cm)
+# def get_absorbance():  # R = Reflection
+#     R = reflection
+#     absorbance = np.log(1 / R)
+#     return absorbance
+
+
+def get_alpha():  # A = Absorbance, d = thickness of the sample (in cm)
+    d = 0.37  # estimated sample thickness
+    A = absorbance
     absorption_coefficient = A / d
     return absorption_coefficient
 
 
-def get_absorbance(R):  # R = Reflection
-    absorbance = math.log(1 / R)
-    return absorbance
-
-
 def get_ordinate(E, a, r):  # E = Energy, a = Absorption Coefficient, r = power factor of the transition mode
-    y = (E*a)**(1/r)
-    return y
+    x = E*a
+    if (x > 0).any():
+        y = x**(1/r)
+        return y
+    else:
+        return 0
 
 
-tauc_spectrum = np.zeros((len(spectrum),2))
-tauc_spectrum[:, 0] = get_energy()
-tauc_spectrum[:, 1] = get_ordinate()
+Energy = get_energy(wavelength_reflection)
+# Absorbance = get_absorbance()
+AbsorptionCoefficient = get_alpha()
+
+r_direct = 1/2
+r_indirect = 2
+
+tauc_spectrum = np.zeros((len(reflection), 2))
+tauc_spectrum[:, 0] = Energy  # hv on x-axis
+tauc_spectrum[:, 1] = get_ordinate(Energy, AbsorptionCoefficient, r_indirect)  # (hv*a)**(1/r) on y-axis
+
+plt.plot(tauc_spectrum[:,0], tauc_spectrum[:,1])
+plt.show()
+
+
+# Calculation:
+
+# Transform Tauc plot to interpolation function
+y = interp1d(tauc_spectrum[:, 0], savgol_filter(tauc_spectrum[:, 1], 51, 3))
+x = np.linspace(tauc_spectrum[0, 0], tauc_spectrum[-1:, 0], 5000)
+
+# Calculate 1st derivative along Tauc plot
+dy = np.diff(y(x), 1)
+dx = np.diff(x, 1)
+y_1d = interp1d(x[:-1], dy / dx)
+
+# Calculate 2nd derivative along Tauc plot
+d2y = np.diff(y(x), 2)
+dx2 = 0.0001
+y_2d = interp1d(x[:-2], d2y / dx2)
+
+# Find point in Tauc plot where 2nd derivative == 0 and gradient is at a maximum
+gradmax = 0.
+for i in range(2, len(x[:-2])):
+    grad = y_1d(x[:-2])[i]
+    if grad > gradmax:
+        gradmax = grad
+    if np.allclose([y_2d(x[:-2])[i]], [0.], atol=0.001) and y(x)[i] > 0.1 * np.amax(
+            tauc_spectrum[:, 1]) and grad >= gradmax:
+        x_0 = x[i]
+        y_0 = y(x)[i]
+
+# Calculate extrapolation line
+m = y_1d(x_0)
+c = y_0 - m * x_0
+
+# Calculate optical gap from extrapolation line
+x_cross = (0 - c) / m
+gap = x_cross
